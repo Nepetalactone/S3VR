665b3224a79652e3a96ef4ffe2e316b6963b129c
sme3863 <stefan.mennel@students.fhv.at>
2014-05-11 15:48:02 +0200

    Basic Project setup

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 S3VR
 ====
+Dependencies:
+
+OpenCV-2.4.9-android-sdk
---
+++ S3ObjectDetector/gen/fhv/s3objectdetector/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package fhv.s3objectdetector;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
