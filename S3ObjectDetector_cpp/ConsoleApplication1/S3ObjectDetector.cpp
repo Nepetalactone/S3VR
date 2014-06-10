535215a21bf59d4740d52f908e4f9d197d0f6291
sme3863 <stefan.mennel@students.fhv.at>
2014-06-10 12:20:29 +0200

    asdf

--- S3ObjectDetector_cpp/ConsoleApplication1/S3ObjectDetector.cpp
+++ S3ObjectDetector_cpp/ConsoleApplication1/S3ObjectDetector.cpp
@@ -137,19 +137,11 @@ int vidStream() {
     cv::Mat image;
 
     const std::string videoStreamAddress = "rtsp://10.0.0.9:1235/"; 
-    /* it may be an address of an mjpeg stream, 
-    e.g. "http://user:pass@cam_address:8081/cgi/mjpg/mjpg.cgi?.mjpg" */
-
-    //open the video stream and make sure it's opened
     if(!vcap.open(videoStreamAddress)) {
         std::cout << "Error opening video stream or file" << std::endl;
         return -1;
     }
 
-    //Create output window for displaying frames. 
-    //It's important to create this window outside of the `for` loop
-    //Otherwise this window will be created automatically each time you call
-    //`imshow(...)`, which is very inefficient. 
     cv::namedWindow("Output Window");
 
     for(;;) {
